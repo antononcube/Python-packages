16576055a0bab12f21c547737155156322743c27
antononcube <antononcube@posteo.net>
2021-11-06 11:36:55 -0400

    feat:Implemented access of elements with both indexes and row- or column keys; both single elements and slices.

--- SSparseMatrix/src/SSparseMatrix.py
+++ SSparseMatrix/src/SSparseMatrix.py
@@ -5,6 +5,10 @@ import scipy
 from scipy import sparse
 import copy
 
+
+# ======================================================================
+# Utilities
+# ======================================================================
 def is_sparse_matrix(obj):
     return isinstance(obj, SSparseMatrix) and scipy.sparse.issparse(obj.sparse_matrix())
 
@@ -14,6 +18,29 @@ def make_from_sparse_matrix(smat):
     return obj.set_sparse_matrix(smat)
 
 
+def is_int_like(x):
+    return scipy.sparse.sputils.isintlike(x)
+
+
+def is_str_like(x):
+    """Is x appropriate as a key into a SSparseMatrix object? Returns True
+    if it can be cast safely to a key.
+    """
+    if scipy.sparse.sputils.issequence(x):
+        return False
+    else:
+        try:
+            if str(x) == x:
+                return True
+            else:
+                return False
+        except TypeError:
+            return False
+
+
+# ======================================================================
+# Class definition
+# ======================================================================
 class SSparseMatrix:
 
     def __init__(self, *args):
@@ -115,6 +142,82 @@ class SSparseMatrix:
                 self.columns_count())
             return None
         return self
+
+    # ------------------------------------------------------------------
+    # Access
+    # ------------------------------------------------------------------
+    def is_key_like(self, x):
+        """Is x appropriate as a key into a SSparseMatrix object? Returns True
+        if it can be cast safely to a key.
+        """
+        return is_str_like(x) and (x in self.row_names() or x in self.column_names())
+
+    def is_row_key_like(self, x):
+        """Is x appropriate as a row key into a SSparseMatrix object? Returns True
+        if it can be cast safely to a key.
+        """
+        return is_str_like(x) and x in self.row_names()
+
+    def is_column_key_like(self, x):
+        """Is x appropriate as a column key into a SSparseMatrix object? Returns True
+        if it can be cast safely to a column key.
+        """
+        return is_str_like(x) and x in self.column_names()
+
+    # ------------------------------------------------------------------
+    # Access
+    # ------------------------------------------------------------------
+    def _get_single_element(self, row, col):
+        if is_int_like(row) and is_int_like(col):
+            return self.sparse_matrix()[row, col]
+        elif row in self.row_names() and col in self.column_names():
+            return self.sparse_matrix()[self.row_names_dict()[row], self.column_names_dict()[col]]
+
+    def _get_submatrix(self, row_slice_arg, col_slice_arg):
+        row_slice = row_slice_arg
+        col_slice = col_slice_arg
+
+        if isinstance(row_slice, list) and is_str_like(row_slice[0]):
+            row_slice = [self.row_names_dict()[x] for x in row_slice]
+
+        if isinstance(col_slice, list) and is_str_like(col_slice[0]):
+            col_slice = [self.column_names_dict()[x] for x in col_slice]
+
+        res = SSparseMatrix(self.sparse_matrix()[row_slice, col_slice])
+
+        if isinstance(row_slice, list):
+            res.set_row_names([self.row_names()[x] for x in row_slice])
+        else:
+            res.set_row_names(self.row_names()[row_slice])
+
+        if isinstance(col_slice, list):
+            res.set_column_names([self.column_names()[x] for x in col_slice])
+        else:
+            res.set_column_names(self.column_names()[col_slice])
+        return res
+
+    def __getitem__(self, key):
+        if isinstance(key, tuple):
+            row = key[0]
+            col = key[1]
+
+            if is_int_like(row) and is_int_like(col) or \
+                    is_str_like(row) and is_str_like(col):
+                return self._get_single_element(row, col)
+            elif isinstance(row, slice) or isinstance(col, slice) or \
+                    isinstance(row, list) or isinstance(col, list):
+                return self._get_submatrix(row, col)
+
+        elif is_int_like(key):
+            return self[key, :]
+        else:
+            raise IndexError("invalid index")
+
+    # ------------------------------------------------------------------
+    # Slicing
+    # ------------------------------------------------------------------
+
+    # def _get_slice(self, i, start, stop, stride, shape):
 
     # ------------------------------------------------------------------
     # Predicates
