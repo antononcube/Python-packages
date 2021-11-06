25aa82cb54bfef6e6edbb2605f3616854b24c11d
antononcube <antononcube@posteo.net>
2021-11-06 11:37:25 -0400

    feat:Multiple examples.

--- SSparseMatrix/SSparseMatrix-experiments.py
+++ SSparseMatrix/SSparseMatrix-experiments.py
@@ -1,32 +1,36 @@
-from SSparseMatrix.SSparseMatrix import SSparseMatrix
+from src.SSparseMatrix import SSparseMatrix
 from scipy import sparse
 import copy
 
-A = sparse.csr_matrix([[1, 2, 0], [0, 0, 3], [4, 0, 5], [4, 10, 5]])
 
-smat = SSparseMatrix()
+smat = SSparseMatrix([[1, 0, 16, 16], [4, 0, 0, 100], [0, 9, 25, 25]])
+smat.set_row_names(["A1", "B2", "C3"])
+smat.set_column_names(["a", "b", "c", "d"])
 
-smat.set_sparse_matrix(A)
+smat.print_matrix(boundary=False)
 
-smat.set_row_names(["a", "b", "c", "d"])
+smat2 = smat.copy().dot(copy.deepcopy(smat).transpose())
+smat2.print_matrix(boundary=True)
 
-smat.set_column_names(["A1", "B2", "C3"])
+print(smat.row_sums())
+print(smat.row_sums_dict())
 
-print(smat.sparse_matrix().todense())
+print(smat.column_sums())
+print(smat.column_sums_dict())
 
-smat.print_matrix()
+print(160 * "=")
 
-print(60 * "-")
+print(smat.sparse_matrix()[0:2,:].todense())
 
-smat.transpose().print_matrix()
+print(smat[0, 3])
+print(smat["A1", "c"])
 
-print(60 * "-")
+print(160 * "-")
+smat[0:2, :].print_matrix()
+smat[:, ["a", "b", "c"]].print_matrix()
+smat[["A1", "B2"], :].print_matrix()
 
-smat.multiply(smat.sparse_matrix()).print_matrix()
-
-print(60 * "-")
-
-smat2 = copy.deepcopy(smat)
-smat2.transpose()
-
-smat.dot(smat2).print_matrix()
+print(160 * "-")
+# It looks like there is a problem with scipy csr_matrix
+#smat.sparse_matrix()[[0,1],[0,1,2]]
+#smat[["A1", "B2"], ["a", "b", "c"]].print_matrix()
