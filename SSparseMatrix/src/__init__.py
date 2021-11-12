98b645f79eabb3a5dec3369f170504526fc667a2
antononcube <antononcube@posteo.net>
2021-11-11 19:21:56 -0500

    build:Better __init__.py.

---
+++ SSparseMatrix/src/__init__.py
@@ -0,0 +1,11 @@
+
+from SSparseMatrix.src.SSparseMatrix import SSparseMatrix
+from SSparseMatrix.src.SSparseMatrix import is_sparse_matrix
+from SSparseMatrix.src.SSparseMatrix import make_s_sparse_matrix
+from SSparseMatrix.src.SSparseMatrix import is_int_like
+from SSparseMatrix.src.SSparseMatrix import is_str_like
+
+from SSparseMatrix.src.version import VERSION
+
+# This ensures VERSION will appear in pydoc
+__version__ = VERSION
