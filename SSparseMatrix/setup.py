d8e7fcfacc6cea16c77b177476e23c9e70f08b32
antononcube <antononcube@posteo.net>
2021-11-11 21:58:32 -0500

    build:Next version.

--- SSparseMatrix/setup.py
+++ SSparseMatrix/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="SSparseMatrix-antononcube",
-    version="0.1.0",
+    version="0.1.1",
     author="Anton Antonov",
     author_email="antononcube@posteo.net",
     description="SSparseMatrix package based on SciPy sparse matrices.",
