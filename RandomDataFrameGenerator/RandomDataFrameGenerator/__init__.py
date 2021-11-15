853084141129f1f404708a279bb7ab18e1ccd03a
antononcube <antononcube@posteo.net>
2021-11-14 21:55:35 -0500

    build:Hopefully better __init__.py.

---
+++ RandomDataFrameGenerator/RandomDataFrameGenerator/__init__.py
@@ -0,0 +1,9 @@
+"""
+Package with functions for creating random tabular data frames
+and lists with random strings, words, pet names, job titles.
+"""
+from RandomDataFrameGenerator.RandomDataFrameGenerator import random_data_frame
+from RandomDataFrameGenerator.RandomFunctions import random_string
+from RandomDataFrameGenerator.RandomFunctions import random_word
+from RandomDataFrameGenerator.RandomFunctions import random_pet_name
+from RandomDataFrameGenerator.RandomFunctions import random_pretentious_job_title
