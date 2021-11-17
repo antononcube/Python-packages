c2412e30e6644dd1e037171b6a60c7de575b7b85
antononcube <antononcube@posteo.net>
2021-11-16 19:49:07 -0500

    docs:Package usage message.

--- SSparseMatrix/SSparseMatrix/__init__.py
+++ SSparseMatrix/SSparseMatrix/__init__.py
@@ -1,4 +1,4 @@
-
+"""SSparseMatrix class : SciPy sparse matrix with named rows and columns."""
 from SSparseMatrix.SSparseMatrix import SSparseMatrix
 from SSparseMatrix.SSparseMatrix import make_s_sparse_matrix
 from SSparseMatrix.SSparseMatrix import is_sparse_matrix
