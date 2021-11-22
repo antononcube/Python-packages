3ccf3e3e299befcacd7c557ed6257b4250292251
antononcube <antononcube@posteo.net>
2021-11-22 11:49:08 -0500

    build:Immediate availability of core functions.

--- LatentSemanticAnalyzer/LatentSemanticAnalyzer/__init__.py
+++ LatentSemanticAnalyzer/LatentSemanticAnalyzer/__init__.py
@@ -1,2 +1,4 @@
 """Latent Semantic Analysis (LSA) package for creating LSA objects and computational workflows with them."""
 from LatentSemanticAnalyzer.LatentSemanticAnalyzer import LatentSemanticAnalyzer
+from LatentSemanticAnalyzer.DocumentTermMatrixConstruction import to_bag_of_words
+from LatentSemanticAnalyzer.DocumentTermMatrixConstruction import document_term_matrix
