f198640e9d180d90fdfdd61332a5365aca1f8ed3
antononcube <antononcube@posteo.net>
2021-11-23 21:26:19 -0500

    feat:Abstract data and loader.

---
+++ LatentSemanticAnalyzer/LatentSemanticAnalyzer/DataLoaders.py
@@ -0,0 +1,29 @@
+import pandas
+import pkg_resources
+
+
+# ===========================================================
+# Load abstracts data
+# ===========================================================
+def load_abstracts_data_frame():
+    """Return a data frame with Titanic data.
+
+    Abstract
+    ----------------
+    Abstracts of Wolfram Research Technology conferences.
+
+    Each row represents one talk.
+
+    :format: A data frame with 567 rows and 5 columns.
+    :field ID: Presentation identifier.
+    :field Name: Presenter(s) name.
+    :field Title: Presentation title.
+    :field Abstract: Abstract of the presentation.
+    :field Track: Presentation track.
+    """
+    # This is a stream-like object. If you want the actual info, call
+    # stream.read()
+    stream = pkg_resources.resource_stream(__name__, 'resources/dfAbstracts.csv')
+    dfData = pandas.read_csv(stream, encoding='latin-1')
+    return dfData
+
