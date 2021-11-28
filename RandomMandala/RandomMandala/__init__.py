64fbbaad010af97f0903b5568f22c33d9de3b040
antononcube <antononcube@posteo.net>
2021-11-27 23:44:40 -0500

    feat:Added figure_to_image.

--- RandomMandala/RandomMandala/__init__.py
+++ RandomMandala/RandomMandala/__init__.py
@@ -1,4 +1,5 @@
 """RandomMandala package"""
 
 from RandomMandala.RandomMandala import RandomMandala
+from RandomMandala.RandomMandala import figure_to_image
 from RandomMandala.RandomFunctions import random_mandala
