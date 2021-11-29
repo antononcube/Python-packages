3f0f8a3791e6bb585197eaa8dd28e6cfe0b6eb7e
antononcube <antononcube@posteo.net>
2021-11-29 12:56:55 -0500

    build:Upgraded for new documentation changes.

--- RandomMandala/setup.py
+++ RandomMandala/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="RandomMandala",
-    version="0.5.0",
+    version="0.5.1",
     author="Anton Antonov",
     author_email="antononcube@posteo.net",
     description="Generator of random mandalas.",
