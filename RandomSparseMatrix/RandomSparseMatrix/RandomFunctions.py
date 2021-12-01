16ae04d3e3a47e756b54026c10177d98c6e9adb8
antononcube <antononcube@posteo.net>
2021-12-01 00:02:55 -0500

    feat:Added warning class.

--- RandomSparseMatrix/RandomSparseMatrix/RandomFunctions.py
+++ RandomSparseMatrix/RandomSparseMatrix/RandomFunctions.py
@@ -77,7 +77,8 @@ def random_sparse_matrix(n_rows=None,
         row_names2 = row_names
         if len(row_names2) < mn_rows:
             warnings.warn(
-                "The specified number of rows is larger than the obtained row names. Adding ordinal suffixes.")
+                "The specified number of rows is larger than the obtained row names. Adding ordinal suffixes.",
+                UserWarning)
             row_names2 = [x + "_" + str(y) for (x, y) in zip(numpy.resize(row_names2, mn_rows), range(mn_rows))]
 
         rmat = rmat.set_row_names(row_names2)
