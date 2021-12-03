3d8bae9720ec996085ef1ef48bf1af9df3ebe3ca
antononcube <antononcube@posteo.net>
2021-12-03 08:59:50 -0500

    build:Version bump.

--- RandomDataGenerators/setup.py
+++ RandomDataGenerators/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="RandomDataGenerators",
-    version="0.2.4",
+    version="0.2.5",
     author="Anton Antonov",
     author_email="antononcube@posteo.net",
     description="Generator of random data frames.",
