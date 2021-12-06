c31fb176da992cfde90a9e2a956622c4748fab6e
antononcube <antononcube@posteo.net>
2021-12-06 13:00:19 -0500

    build:Better setup.py.

--- ChernoffFace/setup.py
+++ ChernoffFace/setup.py
@@ -15,10 +15,13 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=['numpy', 'matplotlib', 'pillow'],
     classifiers=[
+        "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Artistic License",
+        "Intended Audience :: Science/Research",
         "Operating System :: OS Independent",
     ],
+    keywords=["chernoff", "face", "chernoff face", "chernoff face diagram", "multi-dimensional", "visualization"],
     package_data={'': ['resources/*.csv']},
     python_requires='>=3.7',
 )
