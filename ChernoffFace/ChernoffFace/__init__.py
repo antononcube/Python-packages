9242fd0b5ece59932a5854e395b47445e33b54f7
antononcube <antononcube@posteo.net>
2021-12-06 04:51:32 -0500

    build:More pre-declared functions.

--- ChernoffFace/ChernoffFace/__init__.py
+++ ChernoffFace/ChernoffFace/__init__.py
@@ -1,2 +1,4 @@
 """ChernoffFace package"""
+from ChernoffFace.ChernoffFace import chernoff_face_auto_colors
 from ChernoffFace.ChernoffFace import single_chernoff_face
+from ChernoffFace.ChernoffFace import variables_rescale
