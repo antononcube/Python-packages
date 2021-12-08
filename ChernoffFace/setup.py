7e962fd9153e8ee13b5e7b07e5df113d82a2620a
antononcube <antononcube@posteo.net>
2021-12-08 06:31:56 -0500

    build:Version micro increment.

--- ChernoffFace/setup.py
+++ ChernoffFace/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ChernoffFace",
-    version="0.1.3",
+    version="0.1.4",
     author="Anton Antonov",
     author_email="antononcube@posteo.net",
     description="Makes of Chernoff face diagrams.",
