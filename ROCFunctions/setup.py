a0adc1936c5d07fa62e9ec48ba2ed5fe221ab096
antononcube <antononcube@posteo.net>
2022-07-01 15:20:34 -0400

    build:Removed redundant dependencies.

--- ROCFunctions/setup.py
+++ ROCFunctions/setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/antononcube/Python-packages/tree/main/ROCFunctions",
     packages=setuptools.find_packages(),
-    install_requires=['numpy', 'scipy'],
+    install_requires=[],
     classifiers=[
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
