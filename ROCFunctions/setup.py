6c8f29d62c16a11edba3d382962c7093b8b50d48
antononcube <antononcube@posteo.net>
2022-07-01 15:17:33 -0400

    build:Micro version increment.

--- ROCFunctions/setup.py
+++ ROCFunctions/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ROCFunctions",
-    version="0.1.0",
+    version="0.1.1",
     author="Anton Antonov",
     author_email="antononcube@posteo.net",
     description="Receiver Operating Characteristic (ROC) functions package.",
