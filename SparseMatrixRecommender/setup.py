6a6325ad72cfb31746f814edbe3d3af29cc6770a
antononcube <antononcube@posteo.net>
2022-08-19 17:36:26 -0400

    build:Micro version increment.

--- SparseMatrixRecommender/setup.py
+++ SparseMatrixRecommender/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="SparseMatrixRecommender",
-    version="0.1.7",
+    version="0.1.8",
     author="Anton Antonov",
     author_email="antononcube@posteo.net",
     description="Sparse Matrix Recommender package based on SSparseMatrix objects.",
