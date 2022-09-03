4d7f196a0c2f2f7f345b00e0c385387af329342b
antononcube <antononcube@posteo.net>
2022-09-03 15:47:20 -0400

    test:Added shrinking tests and representation tests.

--- TriesWithFrequencies/tests/02-basic-use.py
+++ TriesWithFrequencies/tests/02-basic-use.py
@@ -47,7 +47,7 @@ class BasicUse(unittest.TestCase):
             'r': {'m': {'a': {'n': {'TRIEVALUE': 1.0}, 'TRIEVALUE': 1.0}, 'TRIEVALUE': 0.5},
                   'TRIEVALUE': 0.6666666666666666}, 's': {'k': {'TRIEVALUE': 1.0}, 'TRIEVALUE': 0.3333333333333333},
             'TRIEVALUE': 0.6}, 'e': {'l': {'l': {'TRIEVALUE': 1.0}, 'TRIEVALUE': 0.5},
-                                      's': {'t': {'TRIEVALUE': 1.0}, 'TRIEVALUE': 0.5}, 'TRIEVALUE': 0.4},
+                                     's': {'t': {'TRIEVALUE': 1.0}, 'TRIEVALUE': 0.5}, 'TRIEVALUE': 0.4},
             'TRIEVALUE': 1.0}, 'TRIEVALUE': 1.0}}
 
         self.assertEqual(tr2, tr3)
@@ -59,6 +59,49 @@ class BasicUse(unittest.TestCase):
         # trie node counts are the same for tries with same words and different frequencies
         tr2 = trie_create_by_split([*self.words, *self.words])
         self.assertEqual(trie_node_counts(self.tr), trie_node_counts(tr2))
+
+    def test_shrink_1(self):
+        trShrunk = trie_shrink(self.tr)
+        # trie_form(trShrunk)
+        # TRIEROOT => 5.0
+        # └─b => 5.0
+        #   └─a => 3.0
+        #     └─r => 2.0
+        #       └─man => 1.0
+        #     └─sk => 1.0
+        #   └─e => 2.0
+        #     └─ll => 1.0
+        #     └─st => 1.0
+
+        trExpected = {'TRIEROOT': {'TRIEVALUE': 5.0, 'b': {'TRIEVALUE': 5.0, 'a': {'TRIEVALUE': 3.0,
+                                                                                   'r': {'TRIEVALUE': 2.0,
+                                                                                         'man': {'TRIEVALUE': 1.0}},
+                                                                                   'sk': {'TRIEVALUE': 1.0}},
+                                                           'e': {'TRIEVALUE': 2.0, 'll': {'TRIEVALUE': 1.0},
+                                                                 'st': {'TRIEVALUE': 1.0}}}}}
+        self.assertEqual(trShrunk[TRIE_ROOT]["b"][TRIE_VALUE], len(self.words))
+        self.assertEqual(trShrunk, trExpected)
+
+    def test_shrink_2(self):
+        ptrShrunk = trie_shrink(trie_node_probabilities(self.tr))
+        # trie_form(ptrShrunk)
+        # TRIEROOT => 1.0
+        # └─b => 1.0
+        #   └─a => 0.6
+        #     └─r => 0.6666666666666666
+        #       └─man => 1.0
+        #     └─sk => 1.0
+        #   └─e => 0.4
+        #     └─ll => 1.0
+        #     └─st => 1.0
+
+        trExpected = {'TRIEROOT': {'TRIEVALUE': 1.0, 'b': {'TRIEVALUE': 1.0, 'a': {'TRIEVALUE': 0.6, 'r': {
+            'TRIEVALUE': 0.6666666666666666, 'man': {'TRIEVALUE': 1.0}}, 'sk': {'TRIEVALUE': 1.0}},
+                                                           'e': {'TRIEVALUE': 0.4, 'll': {'TRIEVALUE': 1.0},
+                                                                 'st': {'TRIEVALUE': 1.0}}}}}
+
+        self.assertEqual(ptrShrunk[TRIE_ROOT]["b"][TRIE_VALUE], 1.0)
+        self.assertEqual(ptrShrunk, trExpected)
 
 
 if __name__ == '__main__':
--- TriesWithFrequencies/tests/03-representations.py
+++ TriesWithFrequencies/tests/03-representations.py
@@ -10,8 +10,57 @@ class Representations(unittest.TestCase):
     words = ["bar", "barman", "bask", "bell", "best"]
     tr = trie_create_by_split(words)
 
+    # TRIEROOT => 5.0
+    # └─b => 5.0
+    #   ├─a => 3.0
+    #   │ ├─r => 2.0
+    #   │ │ └─m => 1.0
+    #   │ │   └─a => 1.0
+    #   │ │     └─n => 1.0
+    #   │ └─s => 1.0
+    #   │   └─k => 1.0
+    #   └─e => 2.0
+    #     ├─l => 1.0
+    #     │ └─l => 1.0
+    #     └─s => 1.0
+    #       └─t => 1.0
     def test_representation_1(self):
-        pass
+
+        ptr = trie_node_probabilities(self.tr)
+        # trie_form(ptr) :
+        # TRIEROOT => 1.0
+        # └─b => 1.0
+        #   ├─a => 0.6
+        #   │ ├─r => 0.6666666666666666
+        #   │ │ └─m => 0.5
+        #   │ │   └─a => 1.0
+        #   │ │     └─n => 1.0
+        #   │ └─s => 0.3333333333333333
+        #   │   └─k => 1.0
+        #   └─e => 0.4
+        #     ├─l => 0.5
+        #     │ └─l => 1.0
+        #     └─s => 0.5
+        #       └─t => 1.0
+
+        ptrExpected = {'TRIEROOT': {'b': {'a': {
+            'r': {'m': {'a': {'n': {'TRIEVALUE': 1.0}, 'TRIEVALUE': 1.0}, 'TRIEVALUE': 0.5},
+                  'TRIEVALUE': 0.6666666666666666}, 's': {'k': {'TRIEVALUE': 1.0}, 'TRIEVALUE': 0.3333333333333333},
+            'TRIEVALUE': 0.6}, 'e': {'l': {'l': {'TRIEVALUE': 1.0}, 'TRIEVALUE': 0.5},
+                                     's': {'t': {'TRIEVALUE': 1.0}, 'TRIEVALUE': 0.5}, 'TRIEVALUE': 0.4},
+                                          'TRIEVALUE': 1.0}, 'TRIEVALUE': 1.0}}
+
+        self.assertEqual(ptr, ptrExpected)
+        self.assertEqual(ptr[TRIE_ROOT]["b"][TRIE_VALUE], 1.0)
+
+        trExpected = {'TRIEROOT': {'b': {
+            'a': {'r': {'TRIEVALUE': 2.0, 'm': {'a': {'n': {'TRIEVALUE': 1.0}, 'TRIEVALUE': 1.0}, 'TRIEVALUE': 1.0}},
+                  'TRIEVALUE': 3.0, 's': {'k': {'TRIEVALUE': 1.0}, 'TRIEVALUE': 1.0}}, 'TRIEVALUE': 5.0,
+            'e': {'l': {'l': {'TRIEVALUE': 1.0}, 'TRIEVALUE': 1.0}, 'TRIEVALUE': 2.0,
+                  's': {'t': {'TRIEVALUE': 1.0}, 'TRIEVALUE': 1.0}}}, 'TRIEVALUE': 5.0}}
+
+        self.assertEqual(self.tr[TRIE_ROOT]["b"][TRIE_VALUE], 5.0)
+        self.assertEqual(self.tr, trExpected)
 
 
 if __name__ == '__main__':
