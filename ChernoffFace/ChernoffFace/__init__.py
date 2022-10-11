b0ff732dd377ac2b3968738f324e750bf4b6731e
antononcube <antononcube@posteo.net>
2022-10-11 08:51:18 -0400

    build:Added functions at load-time.

--- ChernoffFace/ChernoffFace/__init__.py
+++ ChernoffFace/ChernoffFace/__init__.py
@@ -1,5 +1,9 @@
 """ChernoffFace package"""
 
+from ChernoffFace.ChernoffFunctions import chernoff_face
+from ChernoffFace.ChernoffFunctions import figure_to_image
 from ChernoffFace.ChernoffFace import chernoff_face_auto_colors
 from ChernoffFace.ChernoffFace import single_chernoff_face
 from ChernoffFace.ChernoffFace import variables_rescale
+from ChernoffFace.DataLoaders import load_usa_arrests_data_frame
+from ChernoffFace.DataLoaders import load_employee_attitude_data_frame
