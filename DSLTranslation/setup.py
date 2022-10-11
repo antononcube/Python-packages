344abd206d225270d0441e1209ae0774413a6072
antononcube <antononcube@posteo.net>
2022-10-11 09:23:32 -0400

    build:Removed wrong/redundant dependencies.

--- DSLTranslation/setup.py
+++ DSLTranslation/setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/antononcube/Python-packages/tree/main/DSLTranslation",
     packages=setuptools.find_packages(),
-    install_requires=["json", "urllib", "pyperclip"],
+    install_requires=["pyperclip"],
     classifiers=[
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
