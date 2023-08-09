83ba4f70ecae51c9cfccdbcf21b2311bb2b446d8
antononcube <antononcube@posteo.net>
2023-08-09 08:43:08 -0400

    feat:Full blown LLM function object (functor) implementation that allows the prompts to be both strings or pure functions.

--- LLMFunctions/LLMFunctions/Functor.py
+++ LLMFunctions/LLMFunctions/Functor.py
@@ -1,17 +1,41 @@
-from collections.abc import Iterable
-from LLMFunctions.Configuration import Configuration
-from LLMFunctions.Evaluator import Evaluator
+from typing import List, Callable, Union, Dict
+import inspect
+
 
 class Functor:
     llm_evaluator = None
     prompt = None
+    args: Union[list, None] = None
+    kwonlyargs: Union[list, None] = None
 
     def __init__(self, llm_evaluator, prompt):
         self.llm_evaluator = llm_evaluator
         self.prompt = prompt
+        if callable(self.prompt):
+            ires = inspect.getfullargspec(self.prompt)
+            self.args = ires[0]
+            self.kwonlyargs = ires[4]
 
     def __call__(self, *args, **dargs):
-        return self.llm_evaluator.eval(*args, **dargs)
+
+        # Deepcopy the evaluator
+        llmEvaluatorLocal = self.llm_evaluator.copy()
+
+        if isinstance(self.prompt, str):
+
+            # LLM evaluate
+            return llmEvaluatorLocal.eval(*args, **dargs)
+
+        elif callable(self.prompt):
+
+            # Filter the prompt function
+            print("prompt function args: ", self.args)
+
+            dargs2 = dict(filter(lambda x: x[0] in self.args, dargs.items()))
+            promptLocal = self.prompt(*args, **dargs2)
+
+            # LLM evaluate
+            return llmEvaluatorLocal.eval(promptLocal, **dargs)
 
     def __str__(self):
         return str(repr(self.prompt))
