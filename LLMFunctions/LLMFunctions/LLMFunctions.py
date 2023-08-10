51bbc6eb9f01e4da8a221f9d63a4a986af856f48
antononcube <antononcube@posteo.net>
2023-08-09 23:54:27 -0400

    feat:Added PaLM configuration.

--- LLMFunctions/LLMFunctions/LLMFunctions.py
+++ LLMFunctions/LLMFunctions/LLMFunctions.py
@@ -1,9 +1,10 @@
 import warnings
-
+import os
 from LLMFunctions.Configuration import Configuration
 from LLMFunctions.Evaluator import Evaluator
 from LLMFunctions.Functor import Functor
 import openai
+import google.generativeai
 import warnings
 
 
@@ -46,14 +47,55 @@ def llm_configuration(spec, **kwargs):
                                                       "stream", "logprobs", "echo", "stop", "presence_penalty",
                                                       "frequency_penalty", "logit_bias",
                                                       "user"],
-                                        response_value_keys=["choices", 0, "text"]
-                                        )
+                                        response_value_keys=["choices", 0, "text"])
         if len(kwargs) > 0:
             confChatGPT = confChatGPT.combine(kwargs)
         return confChatGPT
+    elif isinstance(spec, str) and spec.lower() == 'PaLM'.lower():
+
+        # Set key
+        apiKey = os.environ.get("PALM_API_KEY")
+        google.generativeai.configure(api_key=apiKey)
+
+        # Configuration
+        confPaLM = Configuration(
+            name="palm",
+            api_key=None,
+            api_user_id="user",
+            module="google.generativeai",
+            model="models/text-bison-001",
+            function=google.generativeai.generate_text,
+            temperature=0.2,
+            max_tokens=300,
+            total_probability_cutoff=0.03,
+            prompts=None,
+            prompt_delimiter=" ",
+            stop_tokens=None,
+            argument_renames={"max_tokens": "max_output_tokens",
+                              "stop_tokens": "stop_sequences"},
+            fmt="values",
+            known_params=[
+                "model", "prompt", "temperature", "candidate_count", "max_output_tokens", "top_p", "top_k",
+                "safety_settings", "stop_sequences", "client"
+            ],
+            response_object_attribute = "result",
+            response_value_keys=[])
+
+        # Modify by additional arguments
+        if len(kwargs) > 0:
+            confPaLM = confPaLM.combine(kwargs)
+
+        # Result
+        return confPaLM
+
+    elif isinstance(spec, str) and spec.lower() == 'ChatPaLM'.lower():
+        confChatPaLM = llm_configuration("PaLM")
+        if len(kwargs) > 0:
+            confChatPaLM = confChatPaLM.combine(kwargs)
+        return confChatPaLM
     else:
         warnings.warn("Do not know what to do with given configuration spec.")
-        return llm_configuration('openai', **kwargs)
+        return llm_configuration('OpenAI', **kwargs)
     pass
 
 
