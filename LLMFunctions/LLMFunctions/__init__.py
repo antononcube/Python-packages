34568ec5a60586d83e91ace5c3d9da9e255e5a9e
antononcube <antononcube@posteo.net>
2023-08-12 18:46:40 -0400

    build:Added EvaluatorChatGPT import.

--- LLMFunctions/LLMFunctions/__init__.py
+++ LLMFunctions/LLMFunctions/__init__.py
@@ -1,6 +1,7 @@
 """LLMFunctions"""
 from LLMFunctions.Configuration import Configuration
 from LLMFunctions.Evaluator import Evaluator
+from LLMFunctions.EvaluatorChatPaLM import EvaluatorChatPaLM
 from LLMFunctions.Functor import Functor
 from LLMFunctions.LLMFunctions import llm_configuration
 from LLMFunctions.LLMFunctions import llm_evaluator
