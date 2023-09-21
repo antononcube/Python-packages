cc813e1a81682a8f021dd5fd84616f59e66ac1bf
antononcube <antononcube@posteo.net>
2023-09-21 15:53:23 -0400

    refactor:Removed redundant method definitions.

--- LLMFunctions/LLMFunctions/EvaluatorChatGPT.py
+++ LLMFunctions/LLMFunctions/EvaluatorChatGPT.py
@@ -6,26 +6,6 @@ class EvaluatorChatGPT(EvaluatorChat):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def combine_role_messages(self, messages):
-        # Ensure that all elements of messages are pairs (i.e., key-value pairs)
-        if not all(isinstance(message, dict) for message in messages):
-            raise ValueError("All messages should be key-value pairs")
-
-        # Initialize resMessages with the first message
-        res_messages = [messages[0]]
-
-        # Iterate through the remaining messages
-        for message in messages[1:]:
-            if list(message.keys())[0] == list(res_messages[-1].keys())[0]:
-                last_message = res_messages.pop()
-                combined_value = self.conf.prompt_delimiter.join(
-                    [list(last_message.values())[0], list(message.values())[0]])
-                res_messages.append({list(message.keys())[0]: combined_value})
-            else:
-                res_messages.append(message)
-
-        return res_messages
-
     def prompt_texts_combiner(self, prompt, texts, **kwargs):
         messages = super().prompt_texts_combiner(prompt, texts, **kwargs)
 
@@ -39,25 +19,3 @@ class EvaluatorChatGPT(EvaluatorChat):
                 res_messages.append({"role": k, "content": v})
 
         return res_messages
-
-    def prompt_texts_combiner_first(self, prompt, texts, **args):
-        # Make a list from prompts
-        promptLocal = []
-        if isinstance(prompt, list):
-            promptLocal = prompt
-        elif isinstance(prompt, str):
-            promptLocal = [prompt, ]
-        else:
-            TypeError("Cannot convert prompt argument to a list of strings.")
-
-        # Make a list from texts
-        textsLocal = []
-        if isinstance(texts, list):
-            textsLocal = texts
-        elif isinstance(texts, str):
-            textsLocal = [texts, ]
-        else:
-            TypeError("Cannot convert texts argument to a list of strings.")
-
-        # Result by joining the list string with the context delimiter
-        return self.conf.to_dict()['prompt_delimiter'].join(promptLocal + textsLocal)
