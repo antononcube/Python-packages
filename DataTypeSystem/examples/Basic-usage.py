e3c01bff61f8863e8bf6adece49d86c746d5f479
antononcube <antononcube@posteo.net>
2023-09-24 13:20:37 -0400

    docs:More examples.

--- DataTypeSystem/examples/Basic-usage.py
+++ DataTypeSystem/examples/Basic-usage.py
@@ -1,5 +1,9 @@
-import DataTypeSystem.TypeSystem
+from DataTypeSystem import deduce_type
+from DataTypeSystem import record_types
 from DataTypeSystem.Examiner import Examiner
+from DataTypeSystem.Predicates import is_hash_of_hashes, is_array_of_pairs, is_array_of_hashes
+
+import datetime
 
 from RandomDataGenerators import *
 
@@ -29,3 +33,17 @@ print(ex.deduce_type(dc1))
 print(100 * "-")
 print(list(dc1.items())[0])
 print(type(list(dc1.items())[0]))
+
+print(100 * "=")
+
+dsTable = [
+    {"Date": datetime.date(2020, 12, 23), "Nick": "Watson", "Weekday": 3 / 7},
+    {"Date": datetime.date(2019, 10, 29), "Nick": "Roxy", "Weekday": 2 / 3},
+    {"Date": datetime.date(2020, 8, 6), "Nick": "Jimi", "Weekday": 4 / 5},
+    {"Date": datetime.date(2021, 11, 18), "Nick": "Tucker", "Weekday": 4 / 9},
+]
+
+print(is_array_of_hashes(dsTable))
+ex = Examiner()
+print(ex.is_reshapable(dsTable))
+print(record_types(dsTable))
