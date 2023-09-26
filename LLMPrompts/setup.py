0a21a2fb614d1ef7adedfe3b5c95dfa3e6242853
antononcube <antononcube@posteo.net>
2023-09-26 16:20:56 -0400

    build:Micro version increment.

--- LLMPrompts/setup.py
+++ LLMPrompts/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="LLMPrompts",
-    version="0.1.0",
+    version="0.1.1",
     author="Anton Antonov",
     author_email="antononcube@posteo.net",
     description="Facilitating the creation, storage, retrieval, and curation of LLM prompts.",
