b3215208f472bbeaf31345512b3c66839be237e0
antononcube <antononcube@posteo.net>
2023-10-04 07:43:17 -0400

    build:Micro version increment.

--- DataTypeSystem/setup.py
+++ DataTypeSystem/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="DataTypeSystem",
-    version="0.1.0",
+    version="0.1.1",
     author="Anton Antonov",
     author_email="antononcube@posteo.net",
     description="Data type system for different data structures (arrays, lists of dictionaries, etc.).",
