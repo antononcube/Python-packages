ed3ff12b534990c646a79a9712189bc75605d8a3
antononcube <antononcube@posteo.net>
2024-08-18 22:15:33 -0400

    refactor:Better parameter names and signaturs.

--- QuantileRegression/QuantileRegression/__init__.py
+++ QuantileRegression/QuantileRegression/__init__.py
@@ -1,3 +1,5 @@
 """QuantileRegression class : for given data and probabilities fits a basis of functions."""
-from QuantileRegression.QuantileRegression import QuantileRegressionFit
+from QuantileRegression.QuantileRegression import QuantileRegression
 from QuantileRegression.QuantileRegression import quantile_regression_fit
+from QuantileRegression.QuantileRegression import quantile_regression
+
