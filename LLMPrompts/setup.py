15f885f3800b384b3ae6ea0754337187feec6524
antononcube <antononcube@posteo.net>
2025-06-04 13:02:32 -0400

    build:Using the new setup for packages.

--- LLMPrompts/setup.py
+++ LLMPrompts/setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="LLMPrompts",
     version="0.1.5",
     author="Anton Antonov",
     author_email="antononcube@posteo.net",
     description="Facilitating the creation, storage, retrieval, and curation of LLM prompts.",
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/antononcube/Python-packages/tree/main/LLMPrompts",
     packages=setuptools.find_packages(),
@@ -18,7 +15,6 @@ setuptools.setup(
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD License",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
@@ -26,6 +22,7 @@ setuptools.setup(
     keywords=["openai", "chatgpt", "palm", "prompt", "prompts",
               "large language model", "large language models",
               "llm", "llm prompt", "llm prompts"],
-    package_data={'': ['resources/*.json']},
+    package_data={'LLMPrompts': ['resources/*.json']},
     python_requires='>=3.7',
+    license="BSD-3-Clause"
 )
