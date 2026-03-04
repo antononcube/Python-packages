0c228fbbea4fc5682ac3cac1af7ecf87c0f8be8c
antononcube <antononcube@posteo.net>
2026-03-04 15:59:31 -0500

    build:Micro version increment.

--- LLMFunctionObjects/setup.py
+++ LLMFunctionObjects/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="LLMFunctionObjects",
-    version="0.2.2",
+    version="0.2.3",
     author="Anton Antonov",
     author_email="antononcube@posteo.net",
     description="Large Language Models (LLMs) functions package.",
